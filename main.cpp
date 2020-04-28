0cdaf883c4b519bfbdd0096571ffd42ba038223a
serenatay-hub <64325091+serenatay-hub@users.noreply.github.com>
2020-04-28 10:20:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,4 +7,17 @@ int main()
     Graphe g("fichier1.txt","fichier_poids.txt");
     g.trouver_indice_centralite_vecteur_propre();
     g.afficher();
+
+    bool test = false ;
+    int debut, fin ;
+    while(!test)
+    {
+        std::cout << "Debut:" ;
+        std::cin >> debut ;
+        std::cout << "Fin:";
+        std::cin >> fin ;
+        g.Dikjstra(debut,fin) ;
+        std::cout << "\n" ;
+    }
+    return 0 ;
 }
